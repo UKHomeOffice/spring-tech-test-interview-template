708b6e978b5cceacc1915b13c4a0ace5df4ff148
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2024-02-07 16:56:37 +0000

    General improvements to tests within project (#1)
    This change includes the addition of unit tests for the UserService to assist with interviewees. In addition it also include a change to the file name of the existing test so it represents the fact it is a integration test.
    
    This change also includes rectifying a comment on the seed data to point to the correct record that is overdue.

--- src/main/resources/db/migration/R__SeedData.sql
+++ src/main/resources/db/migration/R__SeedData.sql
@@ -6,9 +6,9 @@ INSERT INTO USERS(name) VALUES
 
 INSERT INTO BOOKS(title, author, return_date, user_id) VALUES
 ('Clean Code: A Handbook of Agile Software Craftsmanship', 'Robert C. Martin', DATEADD(DAY, 14, CURRENT_DATE()), 1),
-('Domain-Driven Design: Tackling Complexity in the Heart of Software', 'Eric Evans', DATEADD(DAY, 15, CURRENT_DATE()), 1), -- overdue
+('Domain-Driven Design: Tackling Complexity in the Heart of Software', 'Eric Evans', DATEADD(DAY, 15, CURRENT_DATE()), 1),
 ('Fundamentals of Software Architecture: An Engineering Approach', 'Mark Richards', DATEADD(DAY, 5, CURRENT_DATE()), 2),
-('Patterns of Enterprise Application Architecture', 'Martin Fowler',  DATEADD(DAY, -15, CURRENT_DATE()), 2),
+('Patterns of Enterprise Application Architecture', 'Martin Fowler',  DATEADD(DAY, -15, CURRENT_DATE()), 2), -- overdue
 ('Effective Java', 'Joshua Bloch', null, null),
 ('Continuous Delivery: Reliable Software Releases through Build, Test, and Deployment Automation', 'Jez Humble', null, null),
 ('Site Reliability Engineering: How Google Runs Production Systems', 'Betsy Beyer', null, null),
